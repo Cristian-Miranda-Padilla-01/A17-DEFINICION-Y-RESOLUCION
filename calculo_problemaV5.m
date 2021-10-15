1ff1b12e17785b908dd08d8232d36dd00c24de50
CristianMirandaPadilla3101 <91565020+CristianMirandaPadilla3101@users.noreply.github.com>
2021-10-14 19:54:33 -0500

    Add files via upload

---
+++ calculo_problemaV5.m
@@ -0,0 +1,38 @@
+% Octave Script
+%School                :Tecnologico de Estudios Superiores de Jilotepec 
+%Specialty             :Ingeniería en Sistemas Computacionales 
+% Title                :Resolución de desigualdades
+% Description          :Script para resolver un problema sobre la cantidad minima de Material para hacer una lata cilindrica usada para envasa aceite
+% Author               :Cristan Miranda Padilla
+% Date                 :202123107    
+% Version              :1
+% Usage                :octave>cd/path/
+%		                   :octave>ResolocionDesigualdades
+% Notes                :Requiere aplicacion octave, usar la linea de comandos
+%                      :https://octaveintro.readthedocs.io/en/latest/index.html
+%limpiar variables
+clear all
+%Uso mínimo de material para una lata cilindrica de aceite
+disp ('Material minimo para hacer una  lata cilindrica para envasar aceite.');
+disp ('');
+%Rango de 0...1 en i = 0.1
+r=0:0.1:1;
+
+%valor de la funcion
+disp ('valor de la funcion');
+ar =@(r) (2*(pi)) * (r.^2) + (2./(r))
+
+%funcion a graficar
+y = (2 * (pi))*(r.^2)+(2./(r))
+
+%dibujar r, y
+plot(r,y);
+
+grid on;
+
+%Titulo
+title(['Material minimo r = ' num2str(r)]);
+%Etiqueta para x
+xlabel(['Min r = ' num2str(r)]);
+%Etiqueta para y
+ylabel(['Min y = ' num2str(ar(r))] );
